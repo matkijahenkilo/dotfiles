c76882f7f788d2ccc0158a429b06b494701ccce7
matkijahenkilo <matkija.henkilo@gmail.com>
2024-06-14 12:06:51 -0300

    Refactor fish functions

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -2,15 +2,9 @@
   programs.fish = {
     enable = true;
     functions = {
-      asd = {
-        body = "yt-dlp -f mp4 $argv[1]";
-      };
-      qwe = {
-        body = "gallery-dl -D ./ $argv[1]";
-      };
-      nanakofetch = {
-        body = "command fastfetch --logo-width 42 --logo-height 25 --logo ~/Pictures/fastfetch.jpg";
-      };
+      asd = "yt-dlp -f mp4 $argv[1]";
+      qwe = "gallery-dl -D ./ $argv[1]";
+      nanakofetch = "command fastfetch --logo-width 42 --logo-height 25 --logo ~/Pictures/fastfetch.jpg";
     };
   };
 }
