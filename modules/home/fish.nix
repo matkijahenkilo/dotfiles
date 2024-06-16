bc678bd5be71eae545d81dde8dbe2025ee085e86
matkijahenkilo <matkija.henkilo@gmail.com>
2024-06-16 07:40:00 -0300

    Add fish function

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -3,6 +3,7 @@
     enable = true;
     functions = {
       asd = "yt-dlp -f mp4 $argv[1]";
+      asdx = "yt-dlp -x --audio-format mp3 $argv[1]";
       qwe = "gallery-dl -D ./ $argv[1]";
       nanakofetch = "command fastfetch --logo-width 42 --logo-height 25 --logo ~/Pictures/fastfetch.jpg";
     };
