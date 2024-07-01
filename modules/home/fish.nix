9af309d885b640c5459c83a297f09ac639939634
matkijahenkilo <matkija.henkilo@gmail.com>
2024-06-30 23:48:12 -0300

    Change fastfetch logo name

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -5,7 +5,7 @@
       asd = "yt-dlp -f mp4 $argv[1]";
       asdx = "yt-dlp -x --audio-format mp3 $argv[1]";
       qwe = "gallery-dl -D ./ $argv[1]";
-      nanakofetch = "command fastfetch --logo-width 42 --logo-height 25 --logo ~/Pictures/fastfetch.jpg";
+      nanakofetch = "command fastfetch --logo-width 42 --logo-height 25 --logo ~/Pictures/.fastfetch.jpg";
     };
   };
 }
