9fc327c055c59a1515762f3294849b4142449772
matkijahenkilo <matkija.henkilo@gmail.com>
2024-07-02 02:39:09 -0300

    Add fish plugin
    and update flake

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -1,6 +1,17 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.fish = {
     enable = true;
+    plugins = [
+      {
+        name = "autopair";
+        src = pkgs.fetchFromGitHub {
+          owner = "jorgebucaran";
+          repo = "autopair.fish";
+          rev = "4d1752ff5b39819ab58d7337c69220342e9de0e2";
+          sha256 = "qt3t1iKRRNuiLWiVoiAYOu+9E7jsyECyIqZJ/oRIT1A=";
+        };
+      }
+    ];
     functions = {
       asd = "yt-dlp -f mp4 $argv[1]";
       asdx = "yt-dlp -x --audio-format mp3 $argv[1]";
