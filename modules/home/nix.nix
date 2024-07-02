969a37d9c6fe246ed328b4e04fd77567655d7b21
matkijahenkilo <matkija.henkilo@gmail.com>
2024-07-02 16:18:55 -0300

    Add additional nix settings

--- modules/home/nix.nix
+++ modules/home/nix.nix
@@ -1,5 +1,18 @@
-{ ... }: {
+{ pkgs, lib, ... }: {
   nix = {
+    package = lib.mkDefault pkgs.nixVersions.latest;
+
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "ca-derivations"
+      ];
+      warn-dirty = false;
+      max-jobs = "auto";
+      cores = 0;
+    };
+
     gc = {
       automatic = true;
       frequency = "weekly";
