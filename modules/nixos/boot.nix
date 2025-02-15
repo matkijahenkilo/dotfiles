888e9a48781d444a19f913aa494c1377f04b99f5
matkijahenkilo <matkija.henkilo@gmail.com>
2025-02-15 19:37:53 -0300

    Set kernel to the latest version

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,8 +1,9 @@
-{ ... }: {
+{ pkgs, ... }: {
   boot = {
     loader = {
       efi.canTouchEfiVariables = true;
       systemd-boot.enable = true;
     };
+    kernelPackages = pkgs.linuxPackages_latest;
   };
 }
