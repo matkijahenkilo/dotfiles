9cc5e2e5c8540cf186a4ec87b60442269afee47a
matkijahenkilo <matkija.henkilo@gmail.com>
2025-02-19 00:26:53 -0300

    Enable hardware.alsa.enablePersistence
    cuz Auto-Mute being enabled by default makes my life a pain

--- modules/nixos/pipewire.nix
+++ modules/nixos/pipewire.nix
@@ -1,7 +1,8 @@
-{ lib, ... }: {
+{ lib, pkgs, ... }: {
   services.pulseaudio.enable = lib.mkForce false;
 
   security.rtkit.enable = true;
+  hardware.alsa.enablePersistence = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
