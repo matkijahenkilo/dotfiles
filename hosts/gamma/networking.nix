3af03477725a92e96d2c6c7b922eddcf4d940787
matkijahenkilo <matkija.henkilo@gmail.com>
2025-03-09 05:57:18 -0300

    Allow kf2 ports

--- hosts/gamma/networking.nix
+++ hosts/gamma/networking.nix
@@ -5,5 +5,15 @@
     hosts = {
       "127.0.0.1" = [ "gamma" ];
     };
+    firewall = {
+      enable = true;
+      allowedUDPPorts = [
+        # kf2
+        7777
+        27015
+        20560
+        123
+      ];
+    };
   };
 }
