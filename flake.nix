d5d5a27f2731d842bec254ef09aa41f6eedbcb11
matkijahenkilo <matkija.henkilo@gmail.com>
2025-05-18 01:48:30 -0300

    Delete 'default' host

--- flake.nix
+++ flake.nix
@@ -77,7 +77,6 @@
     in
     {
       nixosConfigurations = {
-        default = mkHost nixosPkgs ./hosts/default/configuration.nix [ ];
         gamma = mkHost nixosPkgs ./hosts/gamma/configuration.nix [ ];
         quirera = mkHost nixosPkgs ./hosts/quirera/configuration.nix [ ];
       };
