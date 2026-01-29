074bbad96e71378512ac0418d870ec7a7e52b7ae
matkijahenkilo <matkija.henkilo@gmail.com>
2026-01-29 20:38:11 -0300

    Add mpv options

--- modules/home/mpv.nix
+++ modules/home/mpv.nix
@@ -1,9 +1,26 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
   programs.mpv = {
     enable = true;
     config = {
       geometry = "800x600";
+      gpu-api = "vulkan";
+      hwdec = "auto";
+      keep-open = "yes";
+      profile = "high-quality";
+      save-position-on-quit = "yes";
+      target-colorspace-hint = "no";
+      vo = "gpu-next";
     };
+    scripts = with pkgs.mpvScripts; [
+      mpris
+      sponsorblock
+      webtorrent-mpv-hook
+    ];
+  };
+
+  home.file.".mozilla/native-messaging-hosts/ff2mpv.json" = {
+    enable = config.programs.firefox.enable;
+    source = "${pkgs.ff2mpv}/lib/mozilla/native-messaging-hosts/ff2mpv.json";
   };
 }
