f2f554c5700558a43b4eb32125daed842a33e01f
matkijahenkilo <matkija.henkilo@gmail.com>
2026-04-01 10:09:38 -0300

    Fix screen outside of DEs not using full RGB

--- hosts/gamma/gpu.nix
+++ hosts/gamma/gpu.nix
@@ -22,4 +22,17 @@
   };
 
   services.ollama.package = pkgs.ollama-rocm;
+
+  # fix monitor not using full rgb in tty screens
+  hardware.display = {
+    outputs."HDMI-A-1".edid = "edid-custom.bin";
+    edid = {
+      packages = [
+        (pkgs.runCommand "edid-custom" { } ''
+          mkdir -p "$out/lib/firmware/edid"
+          cp ${./edid-custom.bin} $out/lib/firmware/edid/edid-custom.bin
+        '')
+      ];
+    };
+  };
 }
